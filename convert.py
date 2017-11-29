ff367bb27939ea5a400c76e6ed085d1c00f02142
Inês Afonso <afonso_ines@hotmail.com>
2017-11-29 16:17:11 +0000

    somebugs fixed

--- convert.py
+++ convert.py
@@ -6,56 +6,62 @@ import sys
 
 
 for lines in sys.stdin:
+	print(lines)
 	sentences = eval(lines) 
 	recursive(sentences)
+
+
 
 iteration = 1
 myConditions = list()
 
-def recursive(line)
-	if atom (line,TRUE)
+def recursive(line):
+	if atom (line,True):
+		pass
 
-	elif line[0]=='<=>'
+	elif line[0]=='<=>' :
 		equivalence(line[1],line[2])
 
-	elif line[0]== '=>' 
+	elif line[0]== '=>' :
 		implication(line[1],line[2])
 
-	elif line[0]=='or' 
+	elif line[0]=='or' :
 		disjunction(line[1],line[2])	
 
-	elif line[0]=='and'
+	elif line[0]=='and' :
 		conjunction(line[1],line[2])
 
-	elif line[0]=='not' 
-		if neg_atom(line,TRUE)
-		elif negation(line[1])
+	elif line[0]=='not' :
+		if neg_atom(line,True):
+			pass
+		elif negation(line[1]):
+			pass
 
 	return
 
 
 def atom(sentence, value):
-	if len(sentence)>1
-		return FALSE
-	elif len(sentence)==1
-		if value == TRUE
+	if len(sentence)>1:
+		return False
+	elif len(sentence)==1:
+		if value == True:
 			myConditions[iteration].append(sentence)
-		return TRUE
+		return True
 	return
 
 def neg_atom(sentence, value):	
-	if len(sentence)==2 and sentence[0]=='not' and atom(sentence[1])
-			if value == TRUE
-				myConditions[iteration].append(sentence)
-		return TRUE
-	else
-		return FALSE		
+	if len(sentence)==2 and sentence[0]=='not' and atom(sentence[1]):
+		if value == True:
+			myConditions[iteration].append(sentence)
+		return True
+	else:
+		return False		
 	return
 
 
 def equivalence(sentence1, sentence2):
-		implication(sentence1,sentence2)	
-		implication(sentence2,sentence1)
+	implication(sentence1,sentence2)	
+	implication(sentence2,sentence1)
 	return
 	
 def implication(sentence1, sentence2):
@@ -63,12 +69,12 @@ def implication(sentence1, sentence2):
 	return 
 
 def disjuntion(sentence1, sentence2):
-	if (atom(sentence1,0) or neg_atom(sentence1,0)) and (atom(sentence2,0) or neg_atom(sentence2,0)) 
-		if (atom(sentence1,0) and neg_atom(sentence2,0)) or (atom(sentence2,0) and neg_atom(sentence1,0)) # ignora-se.
+	if (atom(sentence1,0) or neg_atom(sentence1,0)) and (atom(sentence2,0) or neg_atom(sentence2,0)):
+		if (atom(sentence1,0) and neg_atom(sentence2,0)) or (atom(sentence2,0) and neg_atom(sentence1,0)):# ignora-se.
 			return
-		else
+		else:
 			myConditions[iteration].append([sentence1, sentence2]) # not sure this is how its done
-	else
+	else:
 		distribution(sentence1, sentence2)
 	return
 
@@ -82,34 +88,37 @@ def distribution(sentence1, sentence2): # MERDAA
 
 	iteration = iteration - 2
 
-		for i in  range(0, len(myConditions[iteration-1]))
-			for j in range(0, len(myConditions[iteration-2]))
-				disjuntion(myConditions[iteration-1][i], myConditions[iteration-2][j])
+	for i in  range(0, len(myConditions[iteration-1])):
+		for j in range(0, len(myConditions[iteration-2])):
+			disjuntion(myConditions[iteration-1][i], myConditions[iteration-2][j])
 
 	
 	return
 	
 
 def conjunction(sentence1, sentence2):
-	recursive(sentence1)
-	recursive(sentence2)
+	if (sentence1 == sentence2)
+		recursive(sentence1)
+	else
+		recursive(sentence1)
+		recursive(sentence2)
 	return 
 
 def negation(sentence):
-	if neg_atom(sentence,0)
+	if neg_atom(sentence,0):
 		myConditions[iteration].append(sentence[1])
 
-	elif sentence[0]=='or'
+	elif sentence[0]=='or':
 		conjunction(('not', sentence1), ('not', sentence2))
 
-	elif sentence[0]=='and' 
+	elif sentence[0]=='and': 
 		disjunction(('not', sentence1), ('not', sentence2))
 
-	elif sentence[0]=='<=>' 
+	elif sentence[0]=='<=>' :
 		conjunction(sentence1, ('not', sentence2))
 		conjunction(sentence2, ('not', sentence1))
 
-	elif sentence[0]=='=>' 
+	elif sentence[0]=='=>' :
 		conjunction(sentence1, ('not', sentence2))
 
 

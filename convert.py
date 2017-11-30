4460489f6824ba50df571580dd49eca9921fe7a5
Inês Afonso <afonso_ines@hotmail.com>
2017-11-30 09:38:41 +0000

    Trying to solve problem on convert. it's not sending the sentences individually

--- convert.py
+++ convert.py
@@ -5,8 +5,10 @@ import sys
 #Vai ler do pipe cada coisa introduzida  e diferenciar cada um
 
 
-def recursive(line, myConditions):
-	
+def recursive(line, myC):
+
+	myConditions[iteration]=myC
+
 	if atom (line):
 		return True
 
@@ -34,20 +36,22 @@ def recursive(line, myConditions):
 
 def atom(sentence):
 	if len(sentence)>1:
+		print('not')
+		print (sentence)
 		return False
 	elif len(sentence)==1:
 		return True
 	return
 
 def neg_atom(sentence):
-	if len(sentence)==2 and sentence[0]=='not' and atom(sentence[1],0):
+	if sentence[0]=='not' and atom(sentence[1]):
 		return True
 	else:
 		return False
 	return
 
 def literal(sentence):
-	if atom(sentence) or neg_atom(sentence):
+	if (atom(sentence) or neg_atom(sentence)):
 		return True
 	else:
 		return False
@@ -64,42 +68,57 @@ def implication(cond, sentence1, sentence2):
 	return
 
 def disjunction(cond, sentence1, sentence2):
+	global iteration
 	if (atom(sentence1) and neg_atom(sentence2) and sentence2[1]==sentence1[0]) or (atom(sentence2) and neg_atom(sentence1) and sentence1[1]==sentence2[0]):
-		# disjunction of A and not A -> delete condition
-		myConditions[iteration].delete(cond)
-		myConditions[iteration].delete(sentence1)
-		myConditions[iteration].delete(sentence2)			
+		# disjunction of A and not A -> remove condition
+		removing = (cond,sentence1,sentence2)
+		myConditions[iteration].remove(removing)	
 		return
 	if (atom(sentence1) and atom(sentence2) and sentence2[1]==sentence1[0]) or (neg_atom(sentence2) and neg_atom(sentence1) and sentence1[1]==sentence2[0]):
 		# condition repeated -> remove one of them
-		myConditions[iteration].delete(cond)
-		myConditions[iteration].delete(sentence2)	
+		removing = (cond,sentence1,sentence2)
+		myConditions[iteration].remove(removing)
+		myConditions[iteration].append(sentence1)	
 		return
 	else:	
-		myConditions[iteration].delete(cond)
-		myConditions[iteration].append(sentence1)
-		myConditions[iteration].append(sentence2)
+		removing = (cond,sentence1,sentence2)
+		myConditions[iteration].remove(removing)
+		appending = (sentence1, sentence2)
+		myConditions[iteration].append(appending)
+
 
 	return
 
 
 def conjunction(cond, sentence1, sentence2):
+	global iteration
+
 	if sentence1 == sentence2:
 		# to equal conjunctions
-		myConditions[iteration].delete(cond)
-		myConditions[iteration].delete(sentence2)
+		removing = (cond,sentence1,sentence2)
+		myConditions[iteration].remove(removing)
+		myConditions[iteration].append(sentence1)
 
 	else:
-		myConditions[iteration].delete(cond)
+		removing = (cond,sentence1,sentence2)
+		myConditions[iteration].remove(removing)
 		myConditions[iteration+1]= myConditions[iteration]
-		myConditions[iteration].delete(sentence1)
-		myConditions[iteration+1].delete(sentence2)
+		myConditions[iteration].append(sentence1)
+		myConditions[iteration+1].append(sentence2)
 		iteration=iteration+1
 	return
 
 def negation(cond, sentence):
+	global iteration
 
-	if sentence[0]=='or':
+	if sentence[0]=='not':
+		removing = (cond,sentence)
+		myConditions[iteration].remove(removing)
+		appending = (sentence[1])
+		myConditions[iteration].append(appending)
+		return True
+
+	elif sentence[0]=='or':
 		new1 = ('not', sentence1)
 		new2 = ('not', sentence2)
 		conjunction('and',new1, new2)
@@ -135,30 +154,23 @@ for line in sys.stdin:
 	myConditions.append(list())
 	myConditions[iteration].append(sentence)
 
-	j=1
-
-	recursive(x, myConditions)
-	for a in range(0, len(myConditions[iteration])):
-		print(d)
-		if not literal(a):
-			print(test)
+	j=0
+	while (j==0):
+		print('here')
+		for i in range(0, len(myConditions)):
+			print('test1')
 			j=0
-
-	if (j==0):
-		(for i in range(0, iteration)):
-			print(a)
-			j=0
-			while (j==0):
-				print(b)
-				for x in range(0, len(myConditions[i])):
-					print(c)
-					recursive(x, myConditions[i])
-				j=1
-				for a in range(0, len(myConditions[i])):
-					print(d)
-					if not literal(a):
-						print(test)
+			x = myConditions[i]
+			while(j==0):
+				for aux in range(0, len(x)):
+					j=1
+					if not literal(x[aux]):
+						recursive(x[aux], x)
+						print(x[aux])
+						print('test4')
 						j=0
+					else :
+						print('ye')
 
 				
 
---
+++ tester.py
@@ -0,0 +1,5 @@
+ola = "('not', 'b')"
+sentence = eval(ola)
+if (sentence[0]=='not'):
+	print('nice')
+	print(len(sentence))
